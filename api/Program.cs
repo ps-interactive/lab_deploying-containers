5139e0064385e282b5ce86341607087590be2c6f
Wes Higbee <wes.mcclure@gmail.com>
2024-07-31 18:42:47 -0500

    simplify

--- api/Program.cs
+++ api/Program.cs
@@ -9,23 +9,17 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.MapFallback(() =>
 {
-    return $"Welcome to your web api application, please try getting weather info here: <a href='/weatherforecast'>/weatherforecast</a>";
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+          new WeatherForecast
+          (
+              DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+              Random.Shared.Next(-20, 55),
+              summaries[Random.Shared.Next(summaries.Length)]
+          ))
+          .ToArray();
+    return forecast;
 });
 
 app.Run();
